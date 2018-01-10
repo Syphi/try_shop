ccb48a3f3f31777bddd474d9e4e201d58cb182e9
Syphi <ralkodenis@outlook.com>
2018-01-10 19:26:56 +0200

    some improvement

--- DBlogic/sqlal_queries.py
+++ DBlogic/sqlal_queries.py
@@ -1,19 +1,47 @@
 import sqlalchemy as sa
+import json
+
+def read_from_file(filename):
+    result = []
+    filename += '.txt'
+    with open(filename, 'r') as f:
+        for line in f:
+            if 'name' in line:
+                data = line.split()
+                result.append(data[2])
+            if 'login' in line:
+                data = line.split()
+                result.append(data[2])
+            if 'password' in line:
+                data = line.split()
+                result.append(data[2])
+    return result
 
 
-class DbSqlalQueries():
+class DbSqlalQueries:
 
-    def __init__(self):
+    def __init__(self, name, login, password):
         try:
-            self.db_string = "postgresql://postgres:revo1917@localhost/shop"
+            self.db_string = 'postgresql://' + login + ':' + password + '@localhost/' + name
             self.db = sa.create_engine(self.db_string)
             self.connection = self.db.connect()
             self.metadata = sa.MetaData()
-            self.orders = sa.Table('orders', self.metadata, autoload=True, autoload_with=self.db)
-            self.customer = sa.Table('customer', self.metadata, autoload=True, autoload_with=self.db)
-            self.product = sa.Table('product', self.metadata, autoload=True, autoload_with=self.db)
-            self.category = sa.Table('category', self.metadata, autoload=True, autoload_with=self.db)
-            self.order_products = sa.Table('order_products', self.metadata, autoload=True, autoload_with=self.db)
+            self.metadata.reflect(self.db)
+
+            self.tables = {}
+            for key, value in self.metadata.tables.items():
+                self.tables[key] = sa.Table(str(key), self.metadata, autoload=True, autoload_with=self.db)
+
+            # self.orders = self.metadata.tables['orders']
+            # self.customer = self.metadata.tables['customer']
+            # self.product = self.metadata.tables['product']
+            # self.category = self.metadata.tables['category']
+            # self.order_products = self.metadata.tables['order_products']
+            # self.orders = sa.Table('orders', self.metadata, autoload=True, autoload_with=self.db)
+            # self.customer = sa.Table('customer', self.metadata, autoload=True, autoload_with=self.db)
+            # self.product = sa.Table('product', self.metadata, autoload=True, autoload_with=self.db)
+            # self.category = sa.Table('category', self.metadata, autoload=True, autoload_with=self.db)
+            # self.order_products = sa.Table('order_products', self.metadata, autoload=True, autoload_with=self.db)
             print('Init class and connection')
         except Exception as e:
             print('shit - no such db')
@@ -23,79 +51,88 @@ class DbSqlalQueries():
         self.connection.close()
         print('Close class and connection')
 
-    def add_category(self, cat_name, image_pass, description, slug, parent_id):
-        insert = sa.insert(self.category)
+    def add_category(self, cat_name, slug, parent_id, image_pass = '', description = ''):
+        insert = sa.insert(self.tables['category']).returning(self.tables['category'])
         insert = insert.values({'cat_name': str(cat_name), 'image': str(image_pass), 'description': str(description),
                                 'slug': str(slug), 'parent_id': parent_id})
-        self.connection.execute(insert)
+        for cat_name in self.connection.execute(insert):
+            print(cat_name)
 
-    def add_product(self, category_id, prod_name, image, description, slug, price_ua, in_stock, other_info):
-        insert = sa.insert(self.product)
+    def add_product(self, category_id, prod_name, slug, price_ua, in_stock, image='', description='', other_info=json):
+        insert = sa.insert(self.tables['product']).returning(self.tables['product'])
         insert = insert.values({'category_id': category_id, 'prod_name': str(prod_name), 'image': str(image),
                                'description': str(description), 'slug': str(slug), 'price_ua': price_ua,
                                'in_stock': in_stock, 'other_info': other_info})
-        self.connection.execute(insert)
+        for category_id in self.connection.execute(insert):
+            print(category_id)
 
-    def add_customer(self, user_name, email, password, phone, shipping_address):
-        insert = sa.insert(self.customer)
+    def add_customer(self, user_name, email, password, phone ='', shipping_address=''):
+        insert = sa.insert(self.tables['customer']).returning(self.tables['product'])
         insert = insert.values({'user_name': str(user_name), 'email': str(email), 'password': str(password),
                                 'phone': phone, 'shipping_address': shipping_address})
-        self.connection.execute(insert)
+        for user_name in self.connection.execute(insert):
+            print(user_name)
 
-    def add_orders(self, customer_id, sum_price, delivery_data_time, payment_type, dict):
-        # add to orders table
-        insert = sa.insert(self.orders)
+    def add_orders(self, customer_id, sum_price, dictionary_with_products, delivery_data_time=None, payment_type=None):
+
+        '''add values to orders table'''
+        insert = sa.insert(self.tables['orders']).returning(self.tables['orders'])
         insert = insert.values({'customer_id': customer_id, 'sum_price': sum_price,
                                 'delivery_data_time': delivery_data_time, 'payment_type': payment_type})
-        self.connection.execute(insert)
+        for customer_id in self.connection.execute(insert):
+            print(customer_id)
 
-        # select last id in orders table
-        select = self.orders.select()
-        select = select.where(sa.and_(self.orders.c.customer_id == customer_id,
-                                      self.orders.c.sum_price == sum_price,
+        '''select last id in orders table by giving params'''
+        select = self.tables['orders'].select()
+        select = select.where(sa.and_(self.tables['orders'].c.customer_id == customer_id,
+                                      self.tables['orders'].c.sum_price == sum_price,
                                       # self.orders.c.delivery_date_time == delivery_data_time, //how to
-                                      self.orders.c.payment_type == payment_type))
+                                      self.tables['orders'].c.payment_type == payment_type))
         result_id = self.connection.execute(select).fetchall()
         result_id = result_id[-1][0]
 
-        print(result_id)
-        print(dict)
-
-        # add to orders_product name
-        insert = self.order_products.insert()
+        ''' parse dictionary_with_products and 
+            add all values to orders_product name'''
+        insert = self.tables['order_products'].insert().returning(self.tables['order_products'])
         insert_list = []
-        for key, value in dict.items():
-            print(result_id, key, value)
+        for key, value in dictionary_with_products.items():
             new_row = {'orders_id': result_id, 'product_id': key, 'number_prod': value}
             insert_list.append(new_row)
 
-        print(insert_list)
-        self.connection.execute(insert, insert_list)
+        for some in self.connection.execute(insert, insert_list):
+            print(some)
 
-    def get_category(self):
-        select = self.category.select()
+    def get_all_categories(self):
+        select = self.tables['category'].select()
         result = self.connection.execute(select).fetchall()
-        print(result)
         return result
 
-    def get_subcategory(self, category_id):
-        select = self.category.select()
-        select = select.where(self.category.c.id == category_id)
+    def get_subcategories_by_id(self, category_id):
+        select = self.tables['category'].select()
+        select = select.where(self.tables['category'].c.parent_id == category_id)
         result = self.connection.execute(select).fetchall()
+        print(result)
 
+    def get_parent_category_by_id(self, category_id):
+        select = self.tables['category'].select()
+        select = select.where(self.tables['category'].c.id == category_id)
+        result = self.connection.execute(select).fetchone()
+
+        print(result)
         result_lst = []
-        result_lst.append(result[0])
-        while result[0][-1] != None:
-            select = self.category.select()
-            select = select.where(self.category.c.id == result[0][-1])
-            result = result = self.connection.execute(select).fetchall()
-            result_lst.append(result[0])
+        result_lst.append(result)
+        while result[-1] != None:
+            select = self.tables['category'].select()
+            select = select.where(self.tables['category'].c.id == result[-1])
+            result = self.connection.execute(select).fetchone()
+            result_lst.append(result)
         print(result_lst)
         return result_lst
 
     def get_products_for_category(self, category_id):
-        select = self.product.select()
-        select = select.where(self.product.c.category_id == category_id)
+        select = self.tables['product'].select()
+        select = select.where(self.tables['product'].c.category_id == category_id)
+        print(self.connection.execute(select).fetchall())
         return self.connection.execute(select).fetchall()
 
     def get_orders_for_customer(self, customer_id):
@@ -108,15 +145,15 @@ class DbSqlalQueries():
         'Order_info': [(order.id, order.customer_id, order.sum_price, order.datetime, order.payment_type) ]}
 
         '''
-        select = self.customer.select().\
-            where(self.customer.c.id == customer_id)
+        select = self.tables['customer'].select().\
+            where(self.tables['customer'].c.id == customer_id)
         client_info = self.connection.execute(select).fetchall()[0]
 
         result_dict = {'Client_info': client_info}
 
-        join = self.orders.join(self.order_products,
-                                self.orders.c.id == self.order_products.c.orders_id)
-        result = sa.select([self.orders, self.order_products]).select_from(join)
+        join = self.tables['orders'].join(self.tables['order_products'],
+                                self.tables['orders'].c.id == self.tables['order_products'].c.orders_id)
+        result = sa.select([self.tables['orders'], self.tables['order_products']]).select_from(join)
         orders_lst = self.connection.execute(result).fetchall()
 
         ids, orders_table = list(), list()
@@ -133,7 +170,6 @@ class DbSqlalQueries():
         result_order_lst = []
         for order in orders_table:
             lst = []
-            print(order)
             lst.append(order)
             lst.append(order_products[order[0]])
             result_order_lst.append(lst)
@@ -142,97 +178,96 @@ class DbSqlalQueries():
         return result_dict
 
     def get_customer_from_login_and_password(self, login, password):
-        select = self.customer.select().\
-            where(self.customer.c.user_name == login).\
-            where(self.customer.c.password == password)
+        select = self.tables['customer'].select().\
+            where(self.tables['customer'].c.user_name == login).\
+            where(self.tables['customer'].c.password == password)
         return self.connection.execute(select).fetchall()
 
     def update_category(self, cat_name_old, cat_name_new, image_new, description_new, slug_new, parent_id_new):
-        update = self.category.update()
-        update = update.where(self.category.c.cat_name == cat_name_old).\
+        update = self.tables['category'].update().returning(self.tables['category'])
+        update = update.where(self.tables['category'].c.cat_name == cat_name_old).\
             values(cat_name=cat_name_new, image=image_new, description=description_new,
                    slug=slug_new, parent_id=parent_id_new)
-        self.connection.execute(update)
+        for cat_name_new in self.connection.execute(update):
+            print(cat_name_new)
 
     def update_customer(self, email_old, new_user_name, new_password, new_phone, new_sh_addr):
-        update = self.customer.update()
-        update = update.where(self.customer.c.email == email_old).\
+        update = self.tables['customer'].update().returning(self.tables['customer'])
+        update = update.where(self.tables['customer'].c.email == email_old).\
             values(user_name=new_user_name, password=new_password, phone=new_phone, shipping_address=new_sh_addr)
-        self.connection.execute(update)
+        for new_user_name in self.connection.execute(update):
+            print(new_user_name)
 
-    def update_product(self, old_prod_name , new_category_id, new_prod_name, new_image, new_description,
+    def update_product(self, old_prod_name, new_category_id, new_prod_name, new_image, new_description,
                        new_slug, new_price_ua, new_in_stock, new_other_info):
-        update = self.product.update()
-        update = update.where(self.product.c.prod_name == old_prod_name).\
+        update = self.tables['product'].update().returning(self.tables['product'])
+        update = update.where(self.tables['product'].c.prod_name == old_prod_name).\
             values(category_id=new_category_id, prod_name=new_prod_name, image=new_image, description=new_description,
                    slug=new_slug, price_ua=new_price_ua, in_stock=new_in_stock, other_info=new_other_info)
-        self.connection.execute(update)
+        for new_category_id in self.connection.execute(update):
+            print(new_category_id)
 
     def update_orders(self, order_id, new_customer_id, new_sum_price, new_delivery_data_time, new_payment_type, new_dict):
-        update = self.orders.update()
-        update = update.where(self.orders.c.id == order_id).\
+        update = self.tables['orders'].update().returning(self.tables['orders'])
+        update = update.where(self.tables['orders'].c.id == order_id).\
             values(customer_id=new_customer_id, sum_price=new_sum_price, delivery_data_time=new_delivery_data_time,
                    payment_type=new_payment_type)
-        self.connection.execute(update)
+        for new_customer_id in self.connection.execute(update):
+            print(new_customer_id)
 
         for key, value in new_dict.items():
-            update = self.order_products.update()
-            update = update.where(self.order_products.c.orders_id == order_id).\
+            update = self.tables['order_products'].update().returning(self.tables['order_products'])
+            update = update.where(self.tables['order_products'].c.orders_id == order_id).\
                 values({'orders_id': order_id, 'product_id': key, 'number_prod': value})
-            self.connection.execute(update)
+            for some in self.connection.execute(update):
+                print(some)
 
     def delete_customer(self, id_customer):
-        select = self.orders.select().where(self.orders.c.customer_id == id_customer)
+        select = self.tables['orders'].select().where(self.tables['orders'].c.customer_id == id_customer)
         result = self.connection.execute(select).fetchall()
-        result_lst = []
         result_lst = [x[0] for x in result]
-        print(result_lst)
 
         for x in result_lst:
             self.delete_order(x)
 
-        delete = self.customer.delete.where(self.customer.c.id == id_customer)
-        self.connection.execute(delete)
+        delete = self.tables['customer'].delete.where(self.tables['customer'].c.id == id_customer).\
+            returning(self.tables['customer'])
+        for id_customer in self.connection.execute(delete):
+            print(id_customer)
 
     def delete_order(self, id_order):
-        delete = self.orders.delete()
-        delete = delete.where(self.orders.c.id == id_order)
-        self.connection.execute(delete)
+        delete = self.tables['orders'].delete().returning(self.tables['orders'])
+        delete = delete.where(self.tables['orders'].c.id == id_order)
+        for id_order in self.connection.execute(delete):
+            print(id_order)
 
-        delete = self.order_products.delete()
-        delete = delete.where(self.order_products.c.orders_id == id_order)
-        self.connection.execute(delete)
+        delete = self.tables['order_products'].delete().returning(self.tables['order_products'])
+        delete = delete.where(self.tables['order_products'].c.orders_id == id_order)
+        for id_order in self.connection.execute(delete):
+            print(id_order)
 
 
-test = DbSqlalQueries()
-# test.add_category('name2', '//pass2//', '2description2', 'slug2', 1)
-# test.add_product(category_id=1, prod_name='phone2', image='//pass//', description='description', slug='//slug//2',
+
+conf = 'conf'
+init_list = read_from_file(conf)
+
+DB = DbSqlalQueries(init_list[0], init_list[1], init_list[2])
+# DB.add_category('name_cat--', '//pass2//', '2description2', 'slug2', 1)
+# DB.add_product(category_id=1, prod_name='phone2', image='//pass//', description='description', slug='//slug//2',
 #                  price_ua=100, in_stock=25, other_info={})
-# test.add_customer(user_name='Denis2', email='2ralko2@gmail.com', password='2qwerty2',
+# DB.add_customer(user_name='Denis2', email='2ralko2@gmail.com', password='2qwerty2',
 #                   phone='0671663238', shipping_address='Kovalski2')
-# test.add_orders(customer_id=1, sum_price=777, delivery_data_time='2018-10-19 10:23:54+02',
-#                 payment_type='cash', dict={1: 1, 5: 40})cd
-# test.get_category().sort(key=lambda x: x[0])
-# test.get_subcategory(4)
-# test.get_products_for_category(1)
-# test.get_orders_for_customer(1)
-# test.get_customer_from_login_and_password('Denis', 'qwerty')
-# test.delete_customer(1)
+# DB.add_orders(customer_id=1, sum_price=777, delivery_data_time='2018-10-19 10:23:54+02',
+#                 payment_type='card', dict={1: 1, 5: 40})
+# DB.get_category().sort(key=lambda x: x[0])
+# DB.get_parent_category_by_id(4)
+DB.get_subcategories_by_id(1)
+# DB.get_products_for_category(1)
+# DB.get_orders_for_customer(1)
+# DB.get_customer_from_login_and_password('Denis', 'qwerty')
+# DB.delete_customer(1)
 
-
-# Pass
-# add_category
-# add_product
-# add_customer
-# add_orders
-# get_category
-# products_for_category
-# orders_for_customer
-# get_customer_from_login_and_password
-# get_subcategories
-
-
-
-# Need to do
-# Update
-# Delete
+def test_get_parent_category_by_id():
+    assert DB.get_parent_category_by_id(4) == [(4, 'Name3', '\\\\pass\\\\', 'description3', 'slug3', 2),
+                                       (2, 'new_categorya', 'new_pass', 'TEXT OF DESCRIPTION', 'new_slug', 1),
+                                       (1, 'categorya', 'pass', 'text', 'slug', None)]
