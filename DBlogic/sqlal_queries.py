54550f5aa676186e78030ddb688c5ccfe1af876c
Syphi <ralkodenis@outlook.com>
2018-01-12 16:45:24 +0200

    remained fixed ENUM

--- DBlogic/sqlal_queries.py
+++ DBlogic/sqlal_queries.py
@@ -1,34 +1,29 @@
 import sqlalchemy as sa
 import json
-import pytest
 
 
 def read_from_file(filename):
     result = []
-    filename += '.txt'
+    filename = '/home/lenin/PycharmProjects/Shop/config/' + filename + '.txt'
     with open(filename, 'r') as f:
         for line in f:
             if 'name' in line:
                 data = line.split()
                 result.append(data[2])
-            if 'login' in line:
+            elif 'login' in line:
                 data = line.split()
                 result.append(data[2])
-            if 'password' in line:
+            elif 'password' in line:
                 data = line.split()
                 result.append(data[2])
     return result
 
-def my_decorator(table_name, table_id, **kwargs):
-    dict_params = kwargs
-
-
 
 class DbSqlalQueries:
 
-    def __init__(self, name, login, password):
+    def __init__(self, conf_lst):
         try:
-            self.db_string = 'postgresql://' + login + ':' + password + '@localhost/' + name
+            self.db_string = 'postgresql://' + conf_lst[1] + ':' + conf_lst[2] + '@localhost/' + conf_lst[0]
             self.db = sa.create_engine(self.db_string)
             self.connection = self.db.connect()
             self.metadata = sa.MetaData()
@@ -38,17 +33,8 @@ class DbSqlalQueries:
             for key, value in self.metadata.tables.items():
                 self.tables[key] = sa.Table(str(key), self.metadata, autoload=True, autoload_with=self.db)
 
-            # self.orders = self.metadata.tables['orders']
-            # self.customer = self.metadata.tables['customer']
-            # self.product = self.metadata.tables['product']
-            # self.category = self.metadata.tables['category']
-            # self.order_products = self.metadata.tables['order_products']
-            # self.orders = sa.Table('orders', self.metadata, autoload=True, autoload_with=self.db)
-            # self.customer = sa.Table('customer', self.metadata, autoload=True, autoload_with=self.db)
-            # self.product = sa.Table('product', self.metadata, autoload=True, autoload_with=self.db)
-            # self.category = sa.Table('category', self.metadata, autoload=True, autoload_with=self.db)
-            # self.order_products = sa.Table('order_products', self.metadata, autoload=True, autoload_with=self.db)
             print('Init class and connection')
+
         except Exception as e:
             print('shit - no such db')
             print(e)
@@ -70,7 +56,7 @@ class DbSqlalQueries:
                                'in_stock': in_stock, 'other_info': other_info})
         return self.connection.execute(insert).fetchone()
 
-    def add_customer(self, user_name, email, password, phone ='', shipping_address=''):
+    def add_customer(self, user_name, email, password, phone='', shipping_address=''):
         insert = sa.insert(self.tables['customer']).returning(self.tables['product'])
         insert = insert.values({'user_name': str(user_name), 'email': str(email), 'password': str(password),
                                 'phone': phone, 'shipping_address': shipping_address})
@@ -116,69 +102,23 @@ class DbSqlalQueries:
         result = self.connection.execute(select).fetchall()
         print(result)
 
-    def get_parent_category_by_id(self, category_id):
-        select = self.tables['category'].select()
-        select = select.where(self.tables['category'].c.id == category_id)
-        result = self.connection.execute(select).fetchone()
-
-        print(result)
-        result_lst = []
-        result_lst.append(result)
-        while result[-1] != None:
-            select = self.tables['category'].select()
-            select = select.where(self.tables['category'].c.id == result[-1])
-            result = self.connection.execute(select).fetchone()
-            result_lst.append(result)
-        print(result_lst)
-        return result_lst
-
     def get_products_for_category(self, category_id):
         select = self.tables['product'].select()
         select = select.where(self.tables['product'].c.category_id == category_id)
         print(self.connection.execute(select).fetchall())
         return self.connection.execute(select).fetchall()
 
+    def get_order_info_from_order_id(self, order_id):
+        select = self.tables['order_products'].select().\
+            where(self.tables['order_products'].c.orders_id == order_id)
+        order_info = self.connection.execute(select).fetchall()
+        return order_info
+
     def get_orders_for_customer(self, customer_id):
-        '''
-        need to give customer and order info
-        use 3 tables customer, orders, order_products
-
-        join with orders.id == orders_products.orders_id
-        :return: dict{{'Client_info': (id, user_name, email, password, phone, addres),
-        'Order_info': [(order.id, order.customer_id, order.sum_price, order.datetime, order.payment_type) ]}
-
-        '''
-        select = self.tables['customer'].select().\
-            where(self.tables['customer'].c.id == customer_id)
-        client_info = self.connection.execute(select).fetchall()[0]
-
-        result_dict = {'Client_info': client_info}
-
-        join = self.tables['orders'].join(self.tables['order_products'],
-                                self.tables['orders'].c.id == self.tables['order_products'].c.orders_id)
-        result = sa.select([self.tables['orders'], self.tables['order_products']]).select_from(join)
-        orders_lst = self.connection.execute(result).fetchall()
-
-        ids, orders_table = list(), list()
-        order_products = dict()
-        for order in orders_lst:
-            if order[0] in ids:
-                order_products[order[0]].append(order[6:])
-            else:
-                ids.append(order[0])
-                orders_table.append(order[:5])
-                order_products[order[0]] = []
-                order_products[order[0]].append(order[6:])
-
-        result_order_lst = []
-        for order in orders_table:
-            lst = []
-            lst.append(order)
-            lst.append(order_products[order[0]])
-            result_order_lst.append(lst)
-
-        result_dict['Orders_info'] = result_order_lst
-        return result_dict
+        select = self.tables['orders'].select().\
+            where(self.tables['orders'].c.customer_id == customer_id)
+        orders = self.connection.execute(select).fetchall()
+        return orders
 
     def get_customer_by_login_and_password(self, login, password):
         select = self.tables['customer'].select().\
@@ -201,14 +141,9 @@ class DbSqlalQueries:
         update = update.where(self.tables['product'].c.id == product_id).values(kwargs)
         return self.connection.execute(update).fetchone()
 
-    def update_orders(self, order_id, **kwargs):
+    def update_order(self, order_id, **kwargs):
         update = self.tables['orders'].update().returning(self.tables['orders'])
         update = update.where(self.tables['orders'].c.id == order_id).values(kwargs)
-        return self.connection.execute(update).fetchone()
-
-    def update_table_by_table_name(self, table_name, table_id, **kwargs):
-        update = self.tables[table_name].update().returning(self.tables[table_name])
-        update = update.where(self.tables[table_name].c.id == table_id).values(kwargs)
         return self.connection.execute(update).fetchone()
 
     def update_order_details(self, order_id, **kwargs):
@@ -221,59 +156,57 @@ class DbSqlalQueries:
         return result_list
 
     def delete_customer(self, id_customer):
-        select = self.tables['orders'].select().where(self.tables['orders'].c.customer_id == id_customer)
-        result = self.connection.execute(select).fetchall()
-        result_lst = [x[0] for x in result]
+        result_lst = []
+        delete_str = sa.text("SELECT id FROM orders WHERE customer_id = :id_customer")
+        result = self.connection.execute(delete_str, id_customer=id_customer)
 
-        for x in result_lst:
-            self.delete_order(x)
+        if result.fetchone() != None:
+            result_lst.append(result.fetchone())
+            for res in result_lst:
+                self.delete_order(res)
 
-        delete = self.tables['customer'].delete.where(self.tables['customer'].c.id == id_customer).\
-            returning(self.tables['customer'])
-        for id_customer in self.connection.execute(delete):
-            print(id_customer)
+        delete_str = sa.text('''DELETE FROM customer WHERE id = :id_customer 
+                             RETURNING customer.id, customer.user_name, customer.email, customer.password,
+                             customer.phone, customer.shipping_address''')
+        result_info = self.connection.execute(delete_str, id_customer=id_customer).fetchone()
+        return result_info
 
     def delete_order(self, id_order):
-        delete = self.tables['orders'].delete().returning(self.tables['orders'])
-        delete = delete.where(self.tables['orders'].c.id == id_order)
-        for id_order in self.connection.execute(delete):
-            print(id_order)
+        delete_str = sa.text('''DELETE FROM order_products WHERE orders_id = :id_order 
+                                                RETURNING order_products.orders_id, order_products.product_id,
+                                                 order_products.number_prod''')
 
-        delete = self.tables['order_products'].delete().returning(self.tables['order_products'])
-        delete = delete.where(self.tables['order_products'].c.orders_id == id_order)
-        for id_order in self.connection.execute(delete):
-            print(id_order)
+        result = self.connection.execute(delete_str, id_order=id_order).fetchall()
 
+        delete_str = sa.text('''DELETE FROM orders WHERE id = :id_order
+                                         RETURNING orders.id, orders.customer_id, orders.sum_price,
+                                         orders.payment_type, orders.delivery_data_time''')
+        result += self.connection.execute(delete_str, id_order=id_order).fetchone()
+        return result
 
 
 conf = 'conf'
 init_list = read_from_file(conf)
 
-DB = DbSqlalQueries(init_list[0], init_list[1], init_list[2])
+DB = DbSqlalQueries(init_list)
 # result = DB.add_category(cat_name='name_cat--', image_pass='//pass2//',
 #                    description='2description2', slug='slug2',  parent_id=1)
 # DB.add_product(category_id=1, prod_name='phone2', image='//pass//', description='description', slug='//slug//2',
 #                  price_ua=100, in_stock=25, other_info={})
 # DB.add_customer(user_name='Denis2', email='2ralko2@gmail.com', password='2qwerty2',
 #                   phone='0671663238', shipping_address='Kovalski2')
-# DB.add_orders(customer_id=1, sum_price=777, delivery_data_time='2018-10-19 10:23:54+02',
-#                 payment_type='card', dict={1: 1, 5: 40})
+# DB.add_orders(customer_id=3, sum_price=777, delivery_data_time='2018-10-19 10:23:54+02',
+#               payment_type='card', dictionary_with_products={1: 1, 5: 40})
 # DB.get_category().sort(key=lambda x: x[0])
 # DB.get_parent_category_by_id(4)
 # DB.get_subcategories_by_id(1)
 # DB.get_products_for_category(1)
 # DB.get_orders_for_customer(1)
-# DB.get_customer_from_login_and_password('Denis', 'qwerty')
+# DB.get_customer_by_login_and_password('Denis', 'qwerty')
 # DB.delete_customer(1)
-DB.update_table_by_table_name('category', 19, cat_name='new_cat_name')
+# DB.update_table_by_table_name('category', 19, cat_name='new_cat_name')
+# result = DB.new_delete_order(63)
+# DB.new_delete_customer(1)
+# print(DB.new_get_orders_for_customer(3))
+# print(DB.get_order_info_from_order_id(65))
 
-@pytest.mark.parametrize("table_name, table_id, one_param, one_param_value, expected",[
-    ('category', 19, 'cat_name', 'name_cat-new', (19, 'name_cat--19', '//pass2//', '2description2', 'slug2', 1))
-])
-def test_update_table_by_table_name(table_name, table_id, one_param, one_param_value, expected):
-    assert DB.update_table_by_table_name(table_name, table_id, one_param=one_param_value) == expected
-
-def test_get_parent_category_by_id():
-    assert DB.get_parent_category_by_id(4) == [(4, 'Name3', '\\\\pass\\\\', 'description3', 'slug3', 2),
-                                       (2, 'new_categorya', 'new_pass', 'TEXT OF DESCRIPTION', 'new_slug', 1),
-                                       (1, 'categorya', 'pass', 'text', 'slug', None)]
---
+++ test/test_sqlal_queries.py
@@ -0,0 +1,55 @@
+from DBlogic import sqlal_queries as sq
+import datetime
+
+init_list = sq.read_from_file('conf')
+DB = sq.DbSqlalQueries(init_list)
+
+
+def test_update_category():
+    trans = DB.connection.begin()
+    try:
+        assert DB.update_category(19, slug='slug_update') == \
+               (19, 'new_cat_name', '//pass2//', '2description2', 'slug_update', 1)
+    finally:
+        trans.rollback()
+
+
+def test_update_customer():
+    trans = DB.connection.begin()
+    try:
+        assert DB.update_customer(3, password='password_update') == \
+               (3,	'Denis', 'ralko@gmail.com', 'password_update', '0671773238', 'Kovalski 5')
+    finally:
+        trans.rollback()
+
+
+def test_update_product():
+    trans = DB.connection.begin()
+    try:
+        assert DB.update_product(5, other_info={'info': 'new'}) == \
+               (5, 1, 'phone2', '//pass//',	'description', '//slug//2',	100, 25, {'info': 'new'})
+    finally:
+        trans.rollback()
+
+
+def test_add_category():
+    trans = DB.connection.begin()
+    try:
+        result = DB.add_category(cat_name='new_name', slug='new_slug', parent_id=1,
+                               image_pass='//new//', description='new_desc')
+        assert result[1:] == ('new_name', '//new//', 'new_desc', 'new_slug', 1)
+    finally:
+        trans.rollback()
+
+
+def test_get_customer_by_login_and_password():
+    assert DB.get_customer_by_login_and_password('Denis', 'qwerty') == \
+           (3, 'Denis', 'ralko@gmail.com', 'qwerty', '0671773238', 'Kovalski 5')
+
+
+def test_delete_customer():
+    trans = DB.connection.begin()
+    try:
+        assert DB.delete_customer(3) == (3, 'Denis', 'ralko@gmail.com', 'qwerty', '0671773238', 'Kovalski 5')
+    finally:
+        trans.rollback()
